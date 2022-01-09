3403f657b7d3cbc1aa0730cdffc114dd54e17855
Chris Jadama <Chrisjadama@gmail.com>
2022-01-09 13:44:43 +0100

    update

--- class15-materials/arrays-fight-to-the-finish/js/main.js
+++ class15-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,26 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
 
+let Arry = ["Techno", "Fast and furnished", "Jon James"];
+
+for (let i = 0; i < Arry.length; i++) {
+  //  document.querySelector("h2").innerText += `\n ${Arry[i]} `;
+}
+
+Arry.forEach((item, i) => {
+  //document.querySelector("h2").innerText += item;
+});
 
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
+let nums = [1, 2, 3];
+nums.forEach((item, i) => {
+  nums[i] = item + 3;
+});
+console.log(nums);
 
+//Find the average of all the numbers from question two
+let sum = 0;
+for (let i = 0; i < nums.length; i++) {
+  sum += nums[i];
+}
 
-//Find the average of all the numbers from question three
+console.log(sum / nums.length);
--- class15-materials/arrays-in-it-to-win-it/js/main.js
+++ class15-materials/arrays-in-it-to-win-it/js/main.js
@@ -1,7 +1,17 @@
 //Create an array of movies with at least three movies.
-
+let movies = ["Harry Potter", "Fast and Furious", "Superman "];
 //Using the array from above, store the first movie in a variable
 
-//Get the length of the original array and store it in a new variable
+superman = movies[0];
 
-//Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
+//Get the length of the original array and store it in a new variable
+let movieLen = movies.length;
+
+//Get the last element in that array and store it in a new variable.
+// What if your array was really large and you didn't know the last index? Would your solution still work?
+let lastEleme = movies[movies.length - 1];
+
+console.log(lastEleme);
+
+let bestColors = ["green", "blue", "red", "yellow"];
+bestColors.forEach((x, i) => console.log(x));
